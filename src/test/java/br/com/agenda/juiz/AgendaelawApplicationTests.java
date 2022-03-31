cd91901cbaf069edb0a9161692306857b6d4d8f8
11Victor <victor.alcantara@unionitdigital.com.br>
2022-03-31 00:48:19 -0300

    Gerando jar da API

--- src/test/java/br/com/agenda/juiz/AgendaelawApplicationTests.java
+++ src/test/java/br/com/agenda/juiz/AgendaelawApplicationTests.java
@@ -1,13 +1,8 @@
 package br.com.agenda.juiz;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AgendaelawApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
